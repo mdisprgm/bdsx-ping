e05f2ade4d8fce77da78a6941ea77b6cd6ee7e0c
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2021-09-20 22:57:23 +0900

    add util

---
+++ utils/command.ts
@@ -0,0 +1,29 @@
+import { Actor } from "bdsx/bds/actor";
+import { Player } from "bdsx/bds/player";
+import { bedrockServer } from "bdsx/launcher";
+export namespace CmdUtil {
+    export const runCmd = bedrockServer.executeCommand;
+    export const runCmdOnConsole = bedrockServer.executeCommandOnConsole;
+
+    export function getPlayerByEntity(actor: Actor) {
+        return actor.isPlayer() ? actor : undefined;
+    }
+
+    export function Feedback(
+        str: string,
+        target: Player | undefined = undefined,
+        log: boolean = true
+    ) {
+        if (log)
+            console.log("[Feedback]", str.replace(/§(\w{1})|(\d{1})/g, ""));
+
+        if (!target) {
+            runCmd(`say ${str}`);
+            return;
+        }
+        runCmd(`tellraw ${target.getName()} {"rawtext":[{"text":"${str}"}]}`);
+    }
+    export function Log(str: string) {
+        console.log("[Feedback]", str.replace(/§(\w{1})|(\d{1})/g, ""));
+    }
+}
